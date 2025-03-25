be66b7d8ee95c9a52470f59497cddd60eadcd089
bubbliiiing <3323290568@qq.com>
2025-03-25 11:01:57 +0000

    Fix bug in scripts

--- scripts/cogvideox_fun/train_control.py
+++ scripts/cogvideox_fun/train_control.py
@@ -51,7 +51,7 @@ from transformers.utils import ContextManagers
 import datasets
 
 current_file_path = os.path.abspath(__file__)
-project_roots = [os.path.dirname(current_file_path), os.path.dirname(os.path.dirname(current_file_path))]
+project_roots = [os.path.dirname(current_file_path), os.path.dirname(os.path.dirname(current_file_path)), os.path.dirname(os.path.dirname(os.path.dirname(current_file_path)))]
 for project_root in project_roots:
     sys.path.insert(0, project_root) if project_root not in sys.path else None
 
