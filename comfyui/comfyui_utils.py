6d94ff661e8ec5cbd056f40a0ff27ecc9c775522
bubbliiiing <3323290568@qq.com>
2025-09-12 08:58:23 +0800

    Update searching path

--- comfyui/comfyui_utils.py
+++ comfyui/comfyui_utils.py
@@ -26,6 +26,7 @@ def to_pil(image):
     raise ValueError(f"Cannot convert {type(image)} to PIL.Image")
 
 def search_model_in_possible_folders(possible_folders, model):
+    model_name = None
     # Check if the model exists in any of the possible folders within folder_paths.models_dir
     for folder in possible_folders:
         candidate_path = os.path.join(folder_paths.models_dir, folder, model)
@@ -57,21 +58,30 @@ def search_sub_dir_in_possible_folders(possible_folders, sub_dir_name="umt5-xxl"
     # Check if the model exists in any of the possible folders within folder_paths.models_dir
     for folder in possible_folders:
         candidate_path = os.path.join(folder_paths.models_dir, folder)
-        new_possible_folders.append(candidate_path)
+        if os.path.exists(candidate_path) and os.path.isdir(candidate_path):
+            new_possible_folders.append(candidate_path)
+            for sub_dir in os.listdir(candidate_path):
+                new_possible_folders.append(os.path.join(candidate_path, sub_dir))
 
     # If model_name is still None, check eas_cache_dir for each possible folder
     if os.path.exists(eas_cache_dir):
         for folder in possible_folders:
             candidate_path = os.path.join(eas_cache_dir, folder)
-            new_possible_folders.append(candidate_path)
+            if os.path.exists(candidate_path) and os.path.isdir(candidate_path):
+                new_possible_folders.append(candidate_path)
+                for sub_dir in os.listdir(candidate_path):
+                    new_possible_folders.append(os.path.join(candidate_path, sub_dir))
 
-    for folder in new_possible_folders:
-        for root, dirs, files in os.walk(folder):
-            if sub_dir_name in dirs or sub_dir_name in files:
-                return os.path.join(root, sub_dir_name)
+    for folder in new_possible_folders + possible_folders:
+        final_possible_folder = os.path.join(folder, sub_dir_name)
+        final_possible_folder_basename = os.path.join(folder, os.path.basename(sub_dir_name))
+        if os.path.exists(final_possible_folder) and os.path.isdir(final_possible_folder):
+            return final_possible_folder
+        if os.path.exists(final_possible_folder_basename) and os.path.isdir(final_possible_folder_basename):
+            return final_possible_folder_basename
 
     print(f"Please download {sub_dir_name} tokenizer model to one of the following directories:")
-    for folder in new_possible_folders:
+    for folder in possible_folders:
         print(f"- {os.path.join(folder_paths.models_dir, folder)}")
         if os.path.exists(eas_cache_dir):
             print(f"- {os.path.join(eas_cache_dir, folder)}")
