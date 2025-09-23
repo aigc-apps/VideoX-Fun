14eb2a0c03dceac4a29cb94c169aaccf467e0b81
bubbliiiing <3323290568@qq.com>
2025-09-23 14:57:28 +0800

    Update chunked loading

--- comfyui/qwenimage/nodes.py
+++ comfyui/qwenimage/nodes.py
@@ -6,42 +6,67 @@ import inspect
 import json
 import os
 
+import comfy.model_management as mm
 import cv2
+import folder_paths
 import numpy as np
 import torch
+from comfy.utils import ProgressBar, load_torch_file
 from diffusers import FlowMatchEulerDiscreteScheduler
 from einops import rearrange
 from omegaconf import OmegaConf
 from PIL import Image
 
-import comfy.model_management as mm
-import folder_paths
-from comfy.utils import ProgressBar, load_torch_file
-
 from ...videox_fun.data.bucket_sampler import (ASPECT_RATIO_512,
                                                get_closest_ratio)
-from ...videox_fun.models import (AutoencoderKLQwenImage,
-                                  Qwen2Tokenizer,
-                                  QwenImageTransformer2DModel, Qwen2_5_VLForConditionalGeneration)
+from ...videox_fun.models import (AutoencoderKLQwenImage, Qwen2_5_VLConfig,
+                                  Qwen2_5_VLForConditionalGeneration,
+                                  Qwen2Tokenizer, QwenImageTransformer2DModel)
 from ...videox_fun.models.cache_utils import get_teacache_coefficients
 from ...videox_fun.pipeline import QwenImagePipeline
-from ...videox_fun.ui.controller import all_cheduler_dict
+from ...videox_fun.utils.fm_solvers import FlowDPMSolverMultistepScheduler
+from ...videox_fun.utils.fm_solvers_unipc import FlowUniPCMultistepScheduler
 from ...videox_fun.utils.fp8_optimization import (
     convert_model_weight_to_float8, convert_weight_dtype_wrapper,
     replace_parameters_by_name)
 from ...videox_fun.utils.lora_utils import merge_lora, unmerge_lora
-from ...videox_fun.utils.utils import (filter_kwargs,
-                                       get_image_to_video_latent,
-                                       get_video_to_video_latent,
-                                       save_videos_grid)
-from ..wan2_1.nodes import get_wan_scheduler
+from ...videox_fun.utils.utils import filter_kwargs
 from ..comfyui_utils import (eas_cache_dir, script_directory,
-                             search_model_in_possible_folders, search_sub_dir_in_possible_folders)
+                             search_model_in_possible_folders,
+                             search_sub_dir_in_possible_folders)
 
 # Used in lora cache
 transformer_cpu_cache       = {}
 # lora path before
 lora_path_before            = ""
+
+def get_qwen_scheduler(sampler_name, shift):
+    Chosen_Scheduler = {
+        "Flow": FlowMatchEulerDiscreteScheduler,
+        "Flow_Unipc": FlowUniPCMultistepScheduler,
+        "Flow_DPM++": FlowDPMSolverMultistepScheduler,
+    }[sampler_name]
+    scheduler_kwargs = {
+        "base_image_seq_len": 256,
+        "base_shift": 0.5,
+        "invert_sigmas": False,
+        "max_image_seq_len": 8192,
+        "max_shift": 0.9,
+        "num_train_timesteps": 1000,
+        "shift": 1.0,
+        "shift_terminal": 0.02,
+        "stochastic_sampling": False,
+        "time_shift_type": "exponential",
+        "use_beta_sigmas": False,
+        "use_dynamic_shifting": True,
+        "use_exponential_sigmas": False,
+        "use_karras_sigmas": False
+    }
+    scheduler_kwargs['shift'] = shift
+    scheduler = Chosen_Scheduler(
+        **filter_kwargs(Chosen_Scheduler, scheduler_kwargs)
+    )
+    return scheduler
 
 class LoadQwenImageTransformerModel:
     @classmethod
@@ -79,7 +104,7 @@ class LoadQwenImageTransformerModel:
                 56,
                 56
             ],
-            "guidance_embeds": True,
+            "guidance_embeds": False,
             "in_channels": 64,
             "joint_attention_dim": 3584,
             "num_attention_heads": 24,
@@ -215,6 +240,9 @@ class LoadQwenImageTextEncoderModel:
         model_path = folder_paths.get_full_path("text_encoders", model_name)
         text_state_dict = load_torch_file(model_path, safe_load=True)
 
+        if not any(k.startswith("model.") for k in text_state_dict.keys()):
+            text_state_dict = {f"model.{k}": v for k, v in text_state_dict.items()}
+
         kwargs = {
             "attention_dropout": 0.0,
             "bos_token_id": 151643,
@@ -347,16 +375,29 @@ class LoadQwenImageTextEncoderModel:
             "vision_token_id": 151654,
             "vocab_size": 152064
         }
-        
-        sig = inspect.signature(Qwen2_5_VLForConditionalGeneration)
-        accepted = {k: v for k, v in kwargs.items() if k in sig.parameters}
-        text_encoder = Qwen2_5_VLForConditionalGeneration(**accepted)
+        config = Qwen2_5_VLConfig(**kwargs)
+        text_encoder = Qwen2_5_VLForConditionalGeneration._from_config(config)
+        text_encoder.state_dict().keys()-text_state_dict.keys()
+        def transform_key(key):
+            key = key.replace("model.", "model.language_model.")
+            key = key.replace("visual.", "model.visual.")
+            return key
+        text_state_dict = {transform_key(k): v for k, v in text_state_dict.items()}
+
         text_encoder.load_state_dict(text_state_dict)
         text_encoder = text_encoder.eval().to(device=offload_device, dtype=weight_dtype)
 
-        possible_folders = ["CogVideoX_Fun", "Fun_Models", "VideoX_Fun", "Wan-AI"] + \
-                [os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "models/Diffusion_Transformer")] # Possible folder names to check
-        tokenizer = Qwen2Tokenizer.from_pretrained(search_sub_dir_in_possible_folders(possible_folders, sub_dir_name="qwen2_tokenizer"))
+        possible_folders = ["CogVideoX_Fun", "Fun_Models", "VideoX_Fun", "Wan-AI", "Qwen"] + \
+            [os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "models/Diffusion_Transformer")] # Possible folder names to check
+        try:
+            tokenizer_path = search_sub_dir_in_possible_folders(possible_folders, sub_dir_name="qwen2_tokenizer")
+        except:
+            try:
+                tokenizer_path = os.path.join(search_sub_dir_in_possible_folders(possible_folders, sub_dir_name="Qwen-Image"), "tokenizer")
+            except:
+                tokenizer_path = search_sub_dir_in_possible_folders(possible_folders, sub_dir_name="Qwen2.5-VL-7B-Instruct")
+
+        tokenizer = Qwen2Tokenizer.from_pretrained(tokenizer_path)
         return (text_encoder, tokenizer)
 
 
@@ -595,10 +636,10 @@ class QwenImageT2VSampler:
                     "STRING_PROMPT", 
                 ),
                 "width": (
-                    "INT", {"default": 832, "min": 64, "max": 2048, "step": 16}
+                    "INT", {"default": 1344, "min": 64, "max": 2048, "step": 16}
                 ),
                 "height": (
-                    "INT", {"default": 480, "min": 64, "max": 2048, "step": 16}
+                    "INT", {"default": 768, "min": 64, "max": 2048, "step": 16}
                 ),
                 "seed": (
                     "INT", {"default": 43, "min": 0, "max": 0xffffffffffffffff}
@@ -617,9 +658,6 @@ class QwenImageT2VSampler:
                 ),
                 "shift": (
                     "INT", {"default": 5, "min": 1, "max": 100, "step": 1}
-                ),
-                "boundary": (
-                    "FLOAT", {"default": 0.875, "min": 0.00, "max": 1.00, "step": 0.001}
                 ),
                 "teacache_threshold": (
                     "FLOAT", {"default": 0.10, "min": 0.00, "max": 1.00, "step": 0.005}
@@ -644,7 +682,7 @@ class QwenImageT2VSampler:
     FUNCTION = "process"
     CATEGORY = "CogVideoXFUNWrapper"
 
-    def process(self, funmodels, prompt, negative_prompt, width, height, seed, steps, cfg, scheduler, shift, boundary, teacache_threshold, enable_teacache, num_skip_start_steps, teacache_offload, cfg_skip_ratio):
+    def process(self, funmodels, prompt, negative_prompt, width, height, seed, steps, cfg, scheduler, shift, teacache_threshold, enable_teacache, num_skip_start_steps, teacache_offload, cfg_skip_ratio):
         global transformer_cpu_cache
         global transformer_high_cpu_cache
         global lora_path_before
@@ -661,7 +699,7 @@ class QwenImageT2VSampler:
         weight_dtype = funmodels['dtype']
 
         # Load Sampler
-        pipeline.scheduler = get_wan_scheduler(scheduler, shift)
+        pipeline.scheduler = get_qwen_scheduler(scheduler, shift)
 
         coefficients = get_teacache_coefficients(model_name) if enable_teacache else None
         if coefficients is not None:
@@ -718,11 +756,11 @@ class QwenImageT2VSampler:
                 true_cfg_scale = cfg,
                 num_inference_steps = steps,
                 comfyui_progressbar = True,
-            ).videos
-            videos = rearrange(sample, "b c t h w -> (b t) h w c")
+            ).images
+            image = torch.Tensor(np.array(sample[0])).unsqueeze(0) / 255
 
             if not funmodels.get("lora_cache", False):
                 print('Unmerge Lora')
                 for _lora_path, _lora_weight in zip(funmodels.get("loras", []), funmodels.get("strength_model", [])):
                     pipeline = unmerge_lora(pipeline, _lora_path, _lora_weight, device="cuda", dtype=weight_dtype)
-        return (videos,)   
+        return (image,)   
