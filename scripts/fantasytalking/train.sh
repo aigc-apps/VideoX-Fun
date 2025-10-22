63737c279ff5ecfcf014161187dee7de9a2ebc8f
bubbliiiing <3323290568@qq.com>
2025-10-22 10:26:50 +0800

    Update train.sh

--- scripts/fantasytalking/train.sh
+++ scripts/fantasytalking/train.sh
@@ -6,7 +6,7 @@ export DATASET_META_NAME="datasets/internal_datasets/metadata_control.json"
 # export NCCL_P2P_DISABLE=1
 NCCL_DEBUG=INFO
 
-accelerate launch --mixed_precision="bf16" --use_fsdp --fsdp_auto_wrap_policy TRANSFORMER_BASED_WRAP --fsdp_transformer_layer_cls_to_wrap=AudioAttentionBlock --fsdp_sharding_strategy "FULL_SHARD" --fsdp_state_dict_type=SHARDED_STATE_DICT --fsdp_backward_prefetch "BACKWARD_PRE" --fsdp_cpu_ram_efficient_loading False  scripts/fantasytalking/train.py \
+accelerate launch --mixed_precision="bf16" scripts/fantasytalking/train.py \
   --config_path="config/wan2.1/wan_civitai.yaml" \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --train_data_dir=$DATASET_NAME \
