27d2178190bf2ced6179f6facaa0ab44673ec6fd
bubbliiiing <3323290568@qq.com>
2025-12-22 15:04:47 +0800

    Update control_context_scale

--- examples/z_image_fun/predict_i2i_tile_2.1.py
+++ examples/z_image_fun/predict_i2i_tile_2.1.py
@@ -76,7 +76,7 @@ control_image       = "asset/low_res.png"
 # The inpaint_image and mask_image is useless in tile model, just set them to None.
 inpaint_image       = None
 mask_image          = None
-control_context_scale = 0.80
+control_context_scale = 0.85
 
 # Please use as detailed a prompt as possible to describe the object that needs to be generated.
 prompt              = "这是一张充满都市气息的户外人物肖像照片。画面中是一位年轻男性，他展现出时尚而自信的形象。人物拥有精心打理的短发发型，两侧修剪得较短，顶部保留一定长度，呈现出流行的Undercut造型。他佩戴着一副时尚的浅色墨镜或透明镜框眼镜，为整体造型增添了潮流感。脸上洋溢着温和友善的笑容，神情放松自然，给人以阳光开朗的印象。他身穿一件经典的牛仔外套，这件单品永不过时，展现出休闲又有型的穿衣风格。牛仔外套的蓝色调与整体氛围十分协调，领口处隐约可见内搭的衣物。照片的背景是典型的城市街景，可以看到模糊的建筑物、街道和行人，营造出繁华都市的氛围。背景经过了恰当的虚化处理，使人物主体更加突出。光线明亮而柔和，可能是白天的自然光，为照片带来清新通透的视觉效果。整张照片构图专业，景深控制得当，完美捕捉了一个现代都市年轻人充满活力和自信的瞬间，展现出积极向上的生活态度。"
