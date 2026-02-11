5afd9ab128a8d3dcc36c9d05be7094ebe4536ffb
bubbliiiing <3323290568@qq.com>
2026-02-11 16:04:53 +0800

    Update avatar training

--- videox_fun/models/longcatvideo_audio_encoder.py
+++ videox_fun/models/longcatvideo_audio_encoder.py
@@ -42,104 +42,6 @@ def _Wav2Vec2PositionalConvEmbedding_init_hack_(self, config):
 Wav2Vec2PositionalConvEmbedding.__init__ = _Wav2Vec2PositionalConvEmbedding_init_hack_
 
 
-class Wav2Vec2ModelWrapper(nn.Module):
-    def __init__(self, config_path, device='cuda', prefix='wav2vec2.'):
-        super(Wav2Vec2ModelWrapper, self).__init__()
-
-        config, model_kwargs = Wav2Vec2Config.from_pretrained(
-                config_path,
-                return_unused_kwargs=True,
-                force_download=False,
-                local_files_only=True,
-            )
-
-        model_path = os.path.join(config_path, 'pytorch_model.bin')
-        state_dict = torch.load(model_path, map_location=device)
-
-        config.name_or_path = config_path
-        config = copy.deepcopy(config)  # We do not want to modify the config inplace in from_pretrained.
-        # config = Wav2Vec2Mode._autoset_attn_implementation(config, use_flash_attention_2=False)
-
-        # init model
-        with torch.device('meta'):
-            model = Wav2Vec2Mode(config)
-
-        # load checkpoint
-        logging.info(f'loading {model_path}')
-        if prefix is not None:
-            state_dict = {i.replace(prefix, ''):state_dict[i] for i in state_dict}
-        
-        model.tie_weights()
-        m, u = model.load_state_dict(state_dict, assign=True, strict=False)
-            
-        model.tie_weights()
-        model.eval()
-
-        self.model = model
-    
-    @property
-    def feature_extractor(self):
-        return self.model.feature_extractor
-
-    @property
-    def dtype(self):
-        """自动获取内部model的dtype"""
-        return next(self.model.parameters()).dtype
-
-    def forward(
-        self,
-        input_values,
-        seq_len,
-        attention_mask=None,
-        mask_time_indices=None,
-        output_attentions=None,
-        output_hidden_states=None,
-        return_dict=None,
-    ):
-        return self.model(
-            input_values,
-            seq_len,
-            attention_mask=attention_mask,
-            mask_time_indices=mask_time_indices,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-    
-    def feature_extract(
-        self,
-        input_values,
-        seq_len,
-    ):
-        extract_features = self.feature_extractor(input_values)
-        extract_features = extract_features.transpose(1, 2)
-        extract_features = linear_interpolation(extract_features, seq_len=seq_len)
-
-        return self.model.feature_extract(
-            input_values,
-            seq_len
-        )
-
-    def encode(
-        self,
-        extract_features,
-        attention_mask=None,
-        mask_time_indices=None,
-        output_attentions=None,
-        output_hidden_states=None,
-        return_dict=None,
-    ):
-
-        return self.model.encode(
-            extract_features,
-            attention_mask=attention_mask,
-            mask_time_indices=mask_time_indices,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-
-
 # the implementation of Wav2Vec2Model is borrowed from
 # https://github.com/huggingface/transformers/blob/HEAD/src/transformers/models/wav2vec2/modeling_wav2vec2.py
 # initialize our encoder with the pre-trained wav2vec 2.0 weights.
@@ -262,4 +164,105 @@ class Wav2Vec2Mode(Wav2Vec2Model_base):
             last_hidden_state=hidden_states,
             hidden_states=encoder_outputs.hidden_states,
             attentions=encoder_outputs.attentions,
+        )
+
+
+class Wav2Vec2ModelWrapper(nn.Module):
+    def __init__(self, config_path, device='cuda', prefix='wav2vec2.'):
+        super(Wav2Vec2ModelWrapper, self).__init__()
+
+        config, model_kwargs = Wav2Vec2Config.from_pretrained(
+                config_path,
+                return_unused_kwargs=True,
+                force_download=False,
+                local_files_only=True,
+            )
+
+        model_path = os.path.join(config_path, 'pytorch_model.bin')
+        state_dict = torch.load(model_path, map_location=device)
+
+        config.name_or_path = config_path
+        config = copy.deepcopy(config)  # We do not want to modify the config inplace in from_pretrained.
+        # config = Wav2Vec2Mode._autoset_attn_implementation(config, use_flash_attention_2=False)
+
+        # init model
+        with torch.device('meta'):
+            model = Wav2Vec2Mode(config)
+
+        # load checkpoint
+        logging.info(f'loading {model_path}')
+        if prefix is not None:
+            state_dict = {i.replace(prefix, ''):state_dict[i] for i in state_dict}
+        
+        model.tie_weights()
+        m, u = model.load_state_dict(state_dict, assign=True, strict=False)
+            
+        model.tie_weights()
+        model.eval()
+
+        self.model = model
+    
+    @property
+    def feature_extractor(self):
+        return self.model.feature_extractor
+
+    @property
+    def dtype(self):
+        return next(self.model.parameters()).dtype
+
+    @property
+    def device(self):
+        return next(self.model.parameters()).device
+
+    def forward(
+        self,
+        input_values,
+        seq_len,
+        attention_mask=None,
+        mask_time_indices=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return self.model(
+            input_values,
+            seq_len,
+            attention_mask=attention_mask,
+            mask_time_indices=mask_time_indices,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+    
+    def feature_extract(
+        self,
+        input_values,
+        seq_len,
+    ):
+        extract_features = self.feature_extractor(input_values)
+        extract_features = extract_features.transpose(1, 2)
+        extract_features = linear_interpolation(extract_features, seq_len=seq_len)
+
+        return self.model.feature_extract(
+            input_values,
+            seq_len
+        )
+
+    def encode(
+        self,
+        extract_features,
+        attention_mask=None,
+        mask_time_indices=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+
+        return self.model.encode(
+            extract_features,
+            attention_mask=attention_mask,
+            mask_time_indices=mask_time_indices,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
         )
