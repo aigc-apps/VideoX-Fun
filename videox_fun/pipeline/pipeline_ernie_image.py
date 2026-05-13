3098d234d2b3d3351f406555fdd997e4bf2d234a
bubbliiiing <3323290568@qq.com>
2026-05-13 18:13:15 +0800

    Update comment

--- videox_fun/pipeline/pipeline_ernie_image.py
+++ videox_fun/pipeline/pipeline_ernie_image.py
@@ -1,3 +1,4 @@
+# Modified from https://github.com/huggingface/diffusers/blob/main/src/diffusers/pipelines/ernie_image/pipeline_ernie_image.py
 # Copyright 2025 Baidu ERNIE-Image Team and The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
